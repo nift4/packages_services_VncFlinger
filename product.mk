8137590de7fe0792bf5b02a8f4c783fd926ce10d
nift4 <nift4@protonmail.com>
2022-09-15 16:02:11 +0200

    external/vncflinger -> packages/services/VncFlinger

--- product.mk
+++ product.mk
@@ -3,9 +3,9 @@ PRODUCT_PACKAGES += \
     VncFlinger
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    external/vncflinger/sepolicy
+    packages/services/VncFlinger/sepolicy
 
 PRODUCT_COPY_FILES += \
-    external/vncflinger/etc/VNC-RemoteInput.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/VNC-RemoteInput.idc \
-    external/vncflinger/etc/privapp_whitelist_vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_vncflinger.xml \
-    external/vncflinger/etc/default-permissions-vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/default-permissions-vncflinger.xml
+    packages/services/VncFlinger/etc/VNC-RemoteInput.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/VNC-RemoteInput.idc \
+    packages/services/VncFlinger/etc/privapp_whitelist_vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_vncflinger.xml \
+    packages/services/VncFlinger/etc/default-permissions-vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/default-permissions-vncflinger.xml

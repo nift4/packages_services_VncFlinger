4ebc16d5aa8c2122f58b9ea9436f8005952a5c8f
Erfan Abdi <erfangplus@gmail.com>
2022-09-29 23:32:31 +0330

    xml: Move permission files to bp
    * Also add idc to system artifacts whitelist

--- product.mk
+++ product.mk
@@ -6,6 +6,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     packages/services/VncFlinger/sepolicy
 
 PRODUCT_COPY_FILES += \
-    packages/services/VncFlinger/etc/VNC-RemoteInput.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/VNC-RemoteInput.idc \
-    packages/services/VncFlinger/etc/privapp_whitelist_vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_vncflinger.xml \
-    packages/services/VncFlinger/etc/default-permissions-vncflinger.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/default-permissions-vncflinger.xml
+    packages/services/VncFlinger/etc/VNC-RemoteInput.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/VNC-RemoteInput.idc
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/usr/idc/VNC-RemoteInput.idc
